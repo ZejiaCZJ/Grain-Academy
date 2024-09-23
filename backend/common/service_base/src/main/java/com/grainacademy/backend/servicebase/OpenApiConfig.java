f227e90410c3c3215d4193f2b4a7bec9d6e04297
Zejia <cainima147@gmail.com>
2024-09-22 21:40:18 -0700

    adding information for swagger ui --stable

---
+++ backend/common/service_base/src/main/java/com/grainacademy/backend/servicebase/OpenApiConfig.java
@@ -0,0 +1,10 @@
+package com.grainacademy.backend.servicebase;
+
+import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
+import org.springdoc.core.customizers.GlobalOpenApiCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+}
--- backend/services/service_edu/src/main/java/com/grainacademy/backend/eduservice/EduApplication.java
+++ backend/services/service_edu/src/main/java/com/grainacademy/backend/eduservice/EduApplication.java
@@ -1,13 +1,24 @@
 package com.grainacademy.backend.eduservice;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@OpenAPIDefinition(info=@Info(
+        title="Grain Academy",
+        version = "1.0",
+        description = "This is an API management platform for the Grain Academy",
+        contact = @Contact(name = "Zejia Cai", url = "https://www.linkedin.com/in/zejia-cai-053259224/", email = "ZejiaCai01@gmail.com"),
+        license = @License(name = "Apache 2.0", url = "https://www.apache.org/licenses/LICENSE-2.0.html")
+        ))
 public class EduApplication {
 
-    //http://localhost:8001/swagger-ui/index.html
+    //http://localhost:8001/doc.html#/home
     public static void main(String[] args) {
         SpringApplication.run(EduApplication.class, args);
     }
